cc0d9b587da7ae90e6b716ceb28ef628b88c0343
Vic Hytyk <victor.hytyk@justanswer.com>
2018-06-29 17:27:26 +0300

    Initial commit

---
+++ FishingHelper/FishingHelper.lua
@@ -0,0 +1,77 @@
+local addonName = 'FishingHelper'
+local cnt0 = 0
+
+local function onSlotUpdate(event, bagId, slotIndex, isNew)
+  local lure = GetFishingLure()
+  local cnt = 0
+  if lure then
+    cnt = select(3, GetFishingLureInfo(lure))
+  else
+    cnt = 0
+  end
+  if( (not isNew and (GetItemType(bagId, slotIndex) == ITEMTYPE_LURE)) and (cnt0 - cnt == 1) )then
+    FishingWindowRect:SetEdgeColor(0, 255, 0, 1)
+    FishingWindowRect:SetCenterColor(0, 255, 0, 1)
+  end
+  cnt0 = cnt
+end
+
+local function onLureCleared(event)
+  -- d("FV:onLureCleared")
+  local lure = GetFishingLure()
+  if lure then
+    cnt0 = select(3, GetFishingLureInfo(lure))
+  end
+end
+
+local function onLureSet(event,lure)
+  -- d("FV:onLureSet")
+  if lure then
+    cnt0 = select(3, GetFishingLureInfo(lure))
+  end
+end
+
+local function onLootReceived(eventCode, receivedBy, itemName, quantity, soundCategory, lootType, self, isPickpocketLoot, questItemIcon, itemId, isStolen)
+
+end
+
+local function onChatterEnd(eventCode)
+  FishingWindowRect:SetEdgeColor(0, 0, 255, 1)
+  FishingWindowRect:SetCenterColor(0, 0, 255, 1)
+end
+
+local function onNoInteractTarget(eventCode)
+  FishingWindowRect:SetEdgeColor(0, 0, 0, 1)
+  FishingWindowRect:SetCenterColor(0, 0, 0, 1)
+end
+
+local function onClientInteractResult(eventCode, result, interactTargetName)
+  if result == CLIENT_INTERACT_RESULT_SUCCESS and interactTargetName == "Fishing Hole" then
+    FishingWindowRect:SetEdgeColor(255, 0, 0, 1)
+    FishingWindowRect:SetCenterColor(255, 0, 0, 1)
+  end
+end
+
+local function eventRegister()
+  EVENT_MANAGER:RegisterForEvent(addonName, EVENT_INVENTORY_SINGLE_SLOT_UPDATE, onSlotUpdate)
+  
+  EVENT_MANAGER:RegisterForEvent(addonName, EVENT_FISHING_LURE_CLEARED, onLureCleared)
+  EVENT_MANAGER:RegisterForEvent(addonName, EVENT_FISHING_LURE_SET, onLureSet)
+
+  EVENT_MANAGER:RegisterForEvent(addonName, EVENT_CHATTER_END, onChatterEnd)
+  EVENT_MANAGER:RegisterForEvent(addonName, EVENT_NO_INTERACT_TARGET, onNoInteractTarget)
+  EVENT_MANAGER:RegisterForEvent(addonName, EVENT_CLIENT_INTERACT_RESULT, onClientInteractResult)
+end
+
+local function onLoaded( event, addon )
+  if ( addon ~= addonName ) then return end
+    EVENT_MANAGER:UnregisterForEvent(addonName, EVENT_ADD_ON_LOADED)
+    eventRegister()
+  local lure = GetFishingLure()
+  if lure then
+    cnt0 = select(3, GetFishingLureInfo(lure))
+  end
+
+end
+
+EVENT_MANAGER:RegisterForEvent(addonName, EVENT_ADD_ON_LOADED,onLoaded)
---
+++ fishing/Program.cs
@@ -0,0 +1,159 @@
+﻿
+using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Windows.Forms;
+
+public class Program
+{
+    public enum FishingState
+    {
+        NoFishing,
+        WaitingForFishing,
+        Fishing,
+        TimeToCatch
+    }
+
+    [DllImport("user32.dll")]
+    public static extern bool GetWindowRect(IntPtr hwnd, ref Rect rectangle);
+
+    [DllImport("user32.dll", CharSet = CharSet.Auto)]
+    static extern IntPtr SendMessage(IntPtr hWnd, UInt32 Msg, IntPtr wParam, IntPtr lParam);
+
+    const UInt32 WM_KEYDOWN = 0x0100;
+    const UInt32 WM_KEYUP = 0x0101;
+    public struct Rect
+    {
+        public int Left { get; set; }
+        public int Top { get; set; }
+        public int Right { get; set; }
+        public int Bottom { get; set; }
+    }
+
+
+    public static IntPtr GetApplicationPtr() { return Process.GetProcessesByName("eso64").First().MainWindowHandle; }
+
+
+    public static void Delay(int milliseconds)
+    {
+        var random = new Random(DateTime.Now.Millisecond);
+        Thread.Sleep(milliseconds + random.Next(-500, 500));
+    }
+    private static string lastStatus = string.Empty;
+
+    public static void LogStatus(FishingState state)
+    {
+        LogStatus($"status - {state}");
+    }
+    public static void LogStatus(string message)
+    {
+        if (message != lastStatus)
+        {
+            Console.WriteLine($"{DateTime.Now.ToLongTimeString()}: {message}");
+            lastStatus = message;
+        }
+    }
+
+    public static FishingState GetState()
+    {
+        Rect rect = new Rect();
+        GetWindowRect(GetApplicationPtr(), ref rect);
+        using (Bitmap bmpScreenCapture = new Bitmap(1, 1))
+        {
+            using (Graphics g = Graphics.FromImage(bmpScreenCapture))
+            {
+                g.CopyFromScreen(rect.Left + 10,
+                    rect.Bottom - 20,
+                    0,
+                    0,
+                    bmpScreenCapture.Size,
+                    CopyPixelOperation.SourceCopy);
+            }
+
+            if (bmpScreenCapture.GetPixel(0, 0).B > 0)
+            {
+                return FishingState.WaitingForFishing;
+            }
+            if (bmpScreenCapture.GetPixel(0, 0).R > 0)
+            {
+                return FishingState.Fishing;
+            }
+            if (bmpScreenCapture.GetPixel(0, 0).G > 0)
+            {
+                return FishingState.TimeToCatch;
+            }
+        }
+        return FishingState.NoFishing;
+    }
+
+    public static void Main(string[] args)
+    {
+        while (true)
+        {
+            Delay(1000);
+            FishingState state = GetState();
+            LogStatus(state);
+            switch (state)
+            {
+                case FishingState.WaitingForFishing:
+                    WaitingCycle();
+                    break;
+                case FishingState.Fishing:
+                    FishingCycle();
+                    break;
+            }
+        }
+    }
+
+    public static void PressKey(Keys key)
+    {
+        SendMessage(GetApplicationPtr(), WM_KEYDOWN, (IntPtr) key, IntPtr.Zero);
+        SendMessage(GetApplicationPtr(), WM_KEYUP, (IntPtr)key, IntPtr.Zero);
+        LogStatus($"{key} pressed");
+    }
+    public static void WaitingCycle()
+    {
+        LogStatus("WaitingCycle  started");
+        while (true)
+        {
+            PressKey(Keys.E);
+            Delay(1000);
+            FishingState state = GetState();
+            LogStatus(state);
+            if (state == FishingState.Fishing || state == FishingState.NoFishing)
+            {
+                return;
+            }
+            
+        }
+    }
+
+    public static void FishingCycle()
+    {
+        LogStatus("FishingCycle started");
+        while (true)
+        {
+            Thread.Sleep(500);
+            FishingState state = GetState();
+            LogStatus(state);
+            if (state == FishingState.TimeToCatch)
+            {
+                PressKey(Keys.E);
+                Delay(2000);
+                PressKey(Keys.R);
+                Delay(2000);
+                state = GetState();
+            }
+            if (state == FishingState.WaitingForFishing)
+            {
+                return;
+            }
+
+        }
+    }
+
+}
